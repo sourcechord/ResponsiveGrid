ab7fd078e99d7df77cc0fea37b5c28b7f4c6a415
minami <sourcechord@gmail.com>
2016-05-24 08:00:42 +0900

    Edit AssemblyInfo

--- ResponsiveGrid.Wpf/Properties/AssemblyInfo.cs
+++ ResponsiveGrid.Wpf/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("ResponsiveGrid.Wpf")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ResponsiveGrid is custom panel control for grid layout system.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SourceChord")]
 [assembly: AssemblyProduct("ResponsiveGrid.Wpf")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]

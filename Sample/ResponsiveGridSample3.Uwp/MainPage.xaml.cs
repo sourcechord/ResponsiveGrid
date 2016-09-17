da158feaa5e287605c69dcc5e3dd5b8662fd0ea8
minami <sourcechord@gmail.com>
2016-09-17 14:25:19 +0900

    Add Sample Projects

---
+++ Sample/ResponsiveGridSample3.Uwp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace ResponsiveGridSample3.Uwp
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void AddGridItem(object sender, RoutedEventArgs e)
+        {
+            rgrid.Children.Add(new Border());
+        }
+
+        private void RemoveGridItem(object sender, RoutedEventArgs e)
+        {
+            var count = rgrid.Children.Count;
+            if (count > 0)
+            {
+                rgrid.Children.RemoveAt(count - 1);
+            }
+        }
+    }
+}
